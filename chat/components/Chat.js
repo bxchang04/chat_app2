f71b726c275f4839a3ceb860e49204c5eb170a18
bxchang04 <42091866+bxchang04@users.noreply.github.com>
2020-02-14 17:07:20 -0500

    5.5 initial commit

--- chat/components/Chat.js
+++ chat/components/Chat.js
@@ -11,7 +11,7 @@ export default class Chat extends Component {
   //why add (props)?
   constructor(props) {
     super(props);
-    // YellowBox.ignoreWarnings(["Setting a timer"]);
+    YellowBox.ignoreWarnings(["Setting a timer"]);
     if (!firebase.apps.length) {
       firebase.initializeApp({
         apiKey: "AIzaSyANcG9zt8msq61vbVyFeAInu7AvegFD8og",
