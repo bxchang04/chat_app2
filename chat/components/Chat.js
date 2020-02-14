8656cbcbfa1e6d7f75f581d6dc1f73490733c534
bxchang04 <42091866+bxchang04@users.noreply.github.com>
2020-02-14 15:21:38 -0500

    5.4 WIP

--- chat/components/Chat.js
+++ chat/components/Chat.js
@@ -1,34 +1,74 @@
-import React, { Component } from 'react';
-import { GiftedChat, Bubble } from 'react-native-gifted-chat'
-import KeyboardSpacer from 'react-native-keyboard-spacer'
-import { View, Platform, StyleSheet, TextInput, TouchableOpacity, Alert, ScrollView, Text } from 'react-native'
+import React, { Component } from "react";
+import { View, Platform, Text, StyleSheet, YellowBox } from "react-native";
+import { GiftedChat, Bubble } from "react-native-gifted-chat";
+import KeyboardSpacer from "react-native-keyboard-spacer";
+
+//Firebase setup
+const firebase = require('firebase');
+require('firebase/firestore');
+
 export default class Chat extends Component {
 
-  state = {
-    messages: [],
+  constructor() {
+    super();
+    YellowBox.ignoreWarnings(["Setting a timer"]);
+    if (!firebase.apps.length) {
+      firebase.initializeApp({
+        apiKey: "AIzaSyANcG9zt8msq61vbVyFeAInu7AvegFD8og",
+        authDomain: "test-30f18.firebaseapp.com",
+        databaseURL: "https://test-30f18.firebaseio.com",
+        projectId: "test-30f18",
+        storageBucket: "test-30f18.appspot.com",
+        messagingSenderId: "320788073310"
+      });
+    }
+
+
+    };
+
+    //differs from repo
+    // this.referenceMessagesUser = null;
+    this.referenceMessages = firebase.firestore().collection('message');
+    //end differ
+    this.state = {
+      messages: [],
+      user: {
+        _id: "",
+        name: "",
+        avatar: ""
+      },
+      loginText: "please wait..."
+    };
   }
 
   componentDidMount() {
-    this.setState({
-      messages: [
-        {
-          _id: 1,
-          text: 'Hello developer',
-          createdAt: new Date(),
-          user: {
-            _id: 2,
-            name: 'React Native',
-            avatar: 'https://placeimg.com/140/140/any',
-          },
-         },
-         {
-          _id: 2,
-          text: `Welcome ${this.props.navigation.state.params.name} to My Chat!`,
-          createdAt: new Date(),
-          system: true,
-         },
-      ]
-    })
+    // listen to authentication events
+    this.authUnsubscribe = firebase.auth().onAuthStateChanged(async user => {
+      if (!user) {
+        await firebase.auth().signInAnonymously();
+      }
+      //update user state with currently active user data
+      this.setState({
+        user: {
+          _id: user.uid,
+          name: this.props.navigation.state.params.name,
+          avatar: "https://placeimg.com/140/140/any"
+        },
+        loginText: "Hello there!",
+      });
+    });
+
+    // create a reference to the active user's documents (messages) -- is this needed? Not in repo.
+    // this.referenceMessages = firebase.firestore().collection('message').where("uid", "==", this.state.uid); //collection name differs, order differs too
+    // listen for collection changes for current user
+    this.unsubscribeMessageUser = this.referenceMessages.onSnapshot(this.onCollectionUpdate);
+  }
+
+  componentWillUnmount() {
+    // stop listening to authentication
+    this.authUnsubscribe();
+    // stop listening for changes -- not in repo. No longer needed?
+    // this.unsubscribeMessageUser();
   }
 
   onSend(messages = []) {
@@ -44,6 +84,38 @@ export default class Chat extends Component {
      };
    };
 
+   //Save message object to Firestore
+   addMessage() {
+     // add a new list to the collection
+     // const message = this.state.messages[0]; //in repo
+     this.referenceMessages.add({
+       _id: message._id,
+       text: message.text,
+       createdAt: message.createdAt,
+       user: message.user
+       // uid: this.state.uid, // why comment this out? Not in repo
+     });
+   }
+
+   //handle changes of data
+   onCollectionUpdate = (querySnapshot) => {
+     const messages = [];
+     // go through each document
+     querySnapshot.forEach((doc) => {
+       // get the QueryDocumentSnapshot's data
+       var messages = doc.data();
+       messages.push({
+         _id: data._id,
+         text: data.text,
+         createdAt: data.createdAt.toDate(),
+         user: data.user
+       });
+     });
+     this.setState({
+       messages,
+    });
+   }
+
   renderBubble(props) {
    return (
    <Bubble
@@ -57,7 +129,7 @@ export default class Chat extends Component {
    )
   }
 
-   //fix background color later?
+   //why is GiftedChat user = _id: 1?
   render() {
    return (
      <View style={{
@@ -65,16 +137,22 @@ export default class Chat extends Component {
        justifyContent: 'center',
        backgroundColor: this.props.navigation.state.params.color}}
      >
+       <Text>{this.state.loggedInText}</Text>
        <GiftedChat
          renderBubble={this.renderBubble.bind(this)}
          messages={this.state.messages}
          onSend={messages => this.onSend(messages)}
-         user={{
-           _id: 1,
-         }}
+         user={{_id: 1,}}
        />
        {Platform.OS === 'android' ? <KeyboardSpacer /> : null }
      </View>
    )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#FFF",
+  }
+});
