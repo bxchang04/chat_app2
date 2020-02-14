b86e75b75121bd57a31fc9e8c77784a53adfb78d
bxchang04 <42091866+bxchang04@users.noreply.github.com>
2020-02-13 19:21:51 -0500

    5.3 start

--- chat/components/Chat.js
+++ chat/components/Chat.js
@@ -1,24 +1,62 @@
 import React, { Component } from 'react';
-// import relevant components from react native
+import { GiftedChat } from 'react-native-gifted-chat'
+import KeyboardSpacer from 'react-native-keyboard-spacer'
 
 import { StyleSheet, Text, View, TextInput, Button  } from 'react-native';
 
 export default class Chat extends Component {
 
-  // Passes user name given in the start screen to the title
-  static navigationOptions = ({ navigation }) => {
-    return {
-      title: navigation.state.params.name,
-    };
-  };
-
-  render() {
-    return (
-      <View style={{flex: 1,
-          justifyContent: 'center',
-          backgroundColor: this.props.navigation.state.params.color}}
-      >
-      </View>
-    )
+  state = {
+    messages: [],
   }
-}
+
+  componentDidMount() {
+     this.setState({
+       messages: [
+         {
+           _id: 1,
+           text: 'Hello developer',
+           createdAt: new Date(),
+           user: {
+             _id: 2,
+             name: 'React Native',
+             avatar: 'https://placeimg.com/140/140/any',
+           },
+         },
+       ],
+     })
+   }
+
+   onSend(messages = []) {
+     this.setState(previousState => ({
+       messages: GiftedChat.append(previousState.messages, messages),
+     }))
+   }
+
+   // Passes user name given in the start screen to the title
+   static navigationOptions = ({ navigation }) => {
+     return {
+       title: navigation.state.params.name,
+     };
+   };
+
+   //fix background color later?
+   render() {
+     return (
+       <View style={{
+         flex: 1,
+         justifyContent: 'center',
+         backgroundColor: this.props.navigation.state.params.color}}
+       >
+         <GiftedChat
+           messages={this.state.messages}
+           onSend={messages => this.onSend(messages)}
+           user={{
+             _id: 1,
+           }}
+         />
+         {Platform.OS === 'android' ? <KeyboardSpacer /> : null }
+       </View>
+     )
+   }
+ }
