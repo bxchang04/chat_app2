bfc12a7c15e1691a140b7de2975b27580ff2a724
bxchang04 <42091866+bxchang04@users.noreply.github.com>
2020-02-13 21:35:27 -0500

    5.3 first submission

--- chat/components/Chat.js
+++ chat/components/Chat.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
-import { GiftedChat } from 'react-native-gifted-chat'
+import { GiftedChat, Bubble } from 'react-native-gifted-chat'
 import KeyboardSpacer from 'react-native-keyboard-spacer'
-
-import { StyleSheet, Text, View, TextInput, Button  } from 'react-native';
-
+import { View, Platform, StyleSheet, TextInput, TouchableOpacity, Alert, ScrollView, Text } from 'react-native'
 export default class Chat extends Component {
 
   state = {
@@ -11,27 +9,33 @@ export default class Chat extends Component {
   }
 
   componentDidMount() {
-     this.setState({
-       messages: [
-         {
-           _id: 1,
-           text: 'Hello developer',
-           createdAt: new Date(),
-           user: {
-             _id: 2,
-             name: 'React Native',
-             avatar: 'https://placeimg.com/140/140/any',
-           },
+    this.setState({
+      messages: [
+        {
+          _id: 1,
+          text: 'Hello developer',
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: 'React Native',
+            avatar: 'https://placeimg.com/140/140/any',
+          },
          },
-       ],
-     })
-   }
+         {
+          _id: 2,
+          text: `Welcome ${this.props.navigation.state.params.name} to My Chat!`,
+          createdAt: new Date(),
+          system: true,
+         },
+      ]
+    })
+  }
 
-   onSend(messages = []) {
-     this.setState(previousState => ({
-       messages: GiftedChat.append(previousState.messages, messages),
-     }))
-   }
+  onSend(messages = []) {
+   this.setState(previousState => ({
+     messages: GiftedChat.append(previousState.messages, messages),
+   }))
+  }
 
    // Passes user name given in the start screen to the title
    static navigationOptions = ({ navigation }) => {
@@ -40,23 +44,37 @@ export default class Chat extends Component {
      };
    };
 
+  renderBubble(props) {
+   return (
+   <Bubble
+     {...props}
+     wrapperStyle={{
+       right: {
+         backgroundColor: '#000'
+       }
+     }}
+   />
+   )
+  }
+
    //fix background color later?
-   render() {
-     return (
-       <View style={{
-         flex: 1,
-         justifyContent: 'center',
-         backgroundColor: this.props.navigation.state.params.color}}
-       >
-         <GiftedChat
-           messages={this.state.messages}
-           onSend={messages => this.onSend(messages)}
-           user={{
-             _id: 1,
-           }}
-         />
-         {Platform.OS === 'android' ? <KeyboardSpacer /> : null }
-       </View>
-     )
-   }
- }
+  render() {
+   return (
+     <View style={{
+       flex: 1,
+       justifyContent: 'center',
+       backgroundColor: this.props.navigation.state.params.color}}
+     >
+       <GiftedChat
+         renderBubble={this.renderBubble.bind(this)}
+         messages={this.state.messages}
+         onSend={messages => this.onSend(messages)}
+         user={{
+           _id: 1,
+         }}
+       />
+       {Platform.OS === 'android' ? <KeyboardSpacer /> : null }
+     </View>
+   )
+  }
+}
